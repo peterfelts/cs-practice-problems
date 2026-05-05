833b98eae2e419dca50126d84b60a4c9d8585d6c
Peter Felts <peezee1978-gethub@yahoo.com>
2026-05-05 07:31:54 -0700

    Adding problem description to Ways to Make Change problem

--- ways_to_make_change.go
+++ ways_to_make_change.go
@@ -5,6 +5,17 @@ import (
 	"sort"
 )
 
+//   Given an array of distinct positive integers representing coin denominations and a
+//   single non-negative integer, n, representing a target amount of
+//   money, write a function that returns the number of ways to make change for
+//   that target amount using the given coin denominations.
+
+//   Example:
+//   n = 6
+//   denoms = [1, 5]
+
+//   expected output: 2 (1x1 + 1x5, 6x1)
+
 func NumberOfWaysToMakeChange(n int, denoms []int) int {
 
 	// build a slice with length up to the largest denomination
